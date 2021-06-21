7e0e1a245fd0351cc804c9581e606d4dd99a5050
Administrator <Administrator@801-t>
2021-06-21 17:33:48 +0900

    javacore01 team share

---
+++ javacore01/src/com/hk/app/JavaWorld.java
@@ -0,0 +1,13 @@
+package com.hk.app;
+
+public class JavaWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("java world!!"); // 자바언어 - 대소문 구분
+		
+		// 다음예제는 "자기이름 - 아는 것이 힘이다" 라고 출력하시오
+
+	}
+
+}
---
+++ javacore01/src/com/hk/app/Know.java
@@ -0,0 +1,12 @@
+package com.hk.app;
+
+public class Know {
+
+	public static void main(String[] args) {
+		
+		System.out.println("라일락-아는 것이 힘이다");
+		System.out.println();  // 자동완성 sysout 입력하고 ctrl+space 누르면
+
+	}
+
+}
